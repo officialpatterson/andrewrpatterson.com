bb0989cd438fbf0ea3e71b4597340d0fc7a2c67d
Andrew Patterson <andrewrpatterson@btinternet.com>
2017-07-13 17:29:14 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+/* Application entry point*/
+
+var express = require('express');
+var app = express();
+var routes = require('./routes.js')
+
+//set the view engine and directory.
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+//define the URL and location of static files
+app.use('/static', express.static('public'));
+
+app.use('/', routes);
+
+app.listen(3000);
